f755f276bdc500c4481ce79a8a98b930ae603d0f
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-05-31 18:25:48 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,15 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+<i>I'm 👩‍🔬 All Movie Rockers!
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+💡 I Am Just A Bot To upload the Links given in main Channel!...😉
+
+Just Add Me To Your Group And Channel And Connect Them And See My Powers!! 🔥🔥⚡️ 
+
+For More Details Click Help and Know More Button Below..
+--> @shreevish 🧑‍
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +46,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@shreevish
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ 🤖 My Name 🔬</b> : <code> 👩‍ All Movie Rockers <code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ 🧑‍ Creator </b> : <b><i><a href="https://t.me/shreevish">꧁★HACKER★꧂</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ 📝 Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ 📚 Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ 🌐 Know More</b> : <i><a href="https://telegra.ph/Av-Auto-Filter-Bot-05-26">Click Me</a></i>
 """
