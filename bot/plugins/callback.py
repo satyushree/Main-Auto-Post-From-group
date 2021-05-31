ab2b99fbccf1fd45429828a2d06bb80729712b51
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-05-31 18:07:15 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1598,17 +1598,17 @@ async def cb_about(bot, update: CallbackQuery):
     if user_id not in VERIFY.get(str(chat_id)):
         return
 
-    text=f"<i><u>Bot's Status</u></i>\n"
-    text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
-    text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/AlbertEinsteinTG/Adv-Filter-Bot-V2">Source</a>"""
+    text=f"<i><u>🤖 Bot's Status</u></i>\n"
+    text+=f"\n<b><i>🏓 Bot's Uptime :</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
+    text+=f"""\n<b><i>🎸 Dev : ➤ ꧁★HACKER★꧂  <a href="https://telegra.ph/Av-Auto-Filter-Bot-05-26</a></i>\n"""
+    text+=f"\n<b><i>💥 Bot Funtion : :</i></b> <i>Auto Channel Link Uploader</i>\n"
+    text+="""\n<b><i>⚡️ Contact :</i></b> <a href="https://telegra.ph/Av-Auto-Filter-Bot-05-26>Here!</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="https://t.me/shreevish"
                 ),
                 
             InlineKeyboardButton
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/shreevish'),
+            InlineKeyboardButton('Know More 🧾', url ='https://telegra.ph/Av-Auto-Filter-Bot-05-26')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('➕ Add me to Your Group!', url='http://t.me/All_Movie_Rockers_BoT?startgroup=true')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
